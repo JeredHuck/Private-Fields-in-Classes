e412ae923562a7e4cb848155ccac5c291464a92f
JeredHuck <jeredhuck@gmail.com>
2023-09-12 20:25:49 -0500

    Exercise 6
    I failed to understand the solution

--- person.js
+++ person.js
@@ -1,5 +1,14 @@
 class Person {
-  // Write code here
+ #ID () {
+  Math.floor(Math.random());
+ }
+
+ get ID () {
+  return this.#ID;
+ }
 }
 
+const me = new Person(Person.Id()); // Not sure exactly what to do here
+
 module.exports = Person;
+
